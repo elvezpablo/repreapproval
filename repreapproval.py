0d084f438a2669e564af3e466192fa28e885d3da
Jeremy Giovannetti <jeremygio.wsd@gmail.com>
2012-12-28 10:28:36 -0800

    list retrieval and links in dev page. list json formatting for readability

--- repreapproval.py
+++ repreapproval.py
@@ -14,11 +14,11 @@ app = webapp2.WSGIApplication(
         ('/api/v[0-9]/humans/add', API.HumansAdd),
         ('/api/v[0-9]/humans/edit', API.HumansEdit),
         ('/api/v[0-9]/humans/delete', API.HumansDelete),
-        ('/api/v[0-9]/humans/*', API.HumansList),
+        ('/api/v[0-9]/humans/list', API.HumansList),
         ('/api/v[0-9]/request/add', API.RequestAdd),
         ('/api/v[0-9]/letters/edit', API.LettersEdit),
         ('/api/v[0-9]/letters/print', API.LettersPrint),
-        ('/api/v[0-9]/request/*', API.RequestList)
+        ('/api/v[0-9]/request/list', API.RequestList)
     ],
     debug=True)
 
