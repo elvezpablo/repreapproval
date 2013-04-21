2c215e7e7a606ff388e16e933fe1958ffd88dcf1
Paul Rangel <prangel@gmail.com>
2013-04-21 09:48:09 -0700

    fixed placeholder text issue on mustache replace in input fields

--- static/js/app.js
+++ static/js/app.js
@@ -54,34 +54,92 @@
 		});	
 		// TODO: do something else if we got here via a post 
 		// as we just added a new letter 
-		this.post("#/", function(context) {
-			//console.log(context);
-			this.title("Hiya! "+current_user.first_name);
-			context.app.swap('');
-			this.load("data/items.json")
-				.then(function(items){
-					context.items = items;
-				})
-				.load('templates/item_detail.handlebars')
-				.then(function(partial){					
-					// NOTE we can have more than one
-					context.partials = {letter_detail : partial}; 
-					context.partial('templates/welcome.handlebars');					
-				});
-		});		
+		// this.post("#/", function(context) {
+		// 	//console.log(context);
+		// 	this.title("Hiya! "+current_user.first_name);
+		// 	context.app.swap('');
+		// 	this.load("data/items.json")
+		// 		.then(function(items){
+		// 			context.items = items;
+		// 		})
+		// 		.load('templates/item_detail.handlebars')
+		// 		.then(function(partial){					
+		// 			// NOTE we can have more than one
+		// 			context.partials = {letter_detail : partial}; 
+		// 			context.partial('templates/welcome.handlebars');					
+		// 		});
+		// });		
 		// the detail route 
-		this.get("#/item/:id",function(context) {
+		this.get("#/edit/:id",function(context) {
+			var context = this;
+			context.data = {};			
+			this.title("Edit Letter");
+			this.load("data/current_user.json")
+				.then(function(user) {
+					context.data.user = user;
+				})
+				.load("data/letter.json")
+				.then(function(letter) {
+					context.data.letter = letter[0];
+				}).render('templates/create.handlebars', context.data)
+				.swap()
+				.then(function(partial){ 
+					PredefinedDatePicker.init();
 
+					$("#createLetterForm").validate({						
+						submitHandler: function(form) {							
+							if($("#createLetterForm").valid())
+								form.submit();
+						}
+					});
+
+					$("#newClientModelFormSubmitBtn").click(function() {						
+						$("#newClientModelForm").submit();
+					});
+
+					$("#newClientModelForm").validate({
+						submitHandler: function(form) {
+							console.log("form valid: "+$("#newClientModelForm").valid());
+							//form.submit();
+							// TODO: ajax here 
+							//
+							// add the new user to the list or show error
+							$("#clientList")
+								.prepend($("<option></option>").attr("value", 2)
+								.text($("#inputFullname").val()));							
+							// TODO: show error
+							// 
+							// close the modal
+							$("#newClientModal").modal("hide");
+							// set the selected index of the users to the new user
+							$("#clientList")[0].selectedIndex = 0;
+						}
+					});
+
+					// formats the number input
+					$("#purchasePrice").change(function(){		
+						console.log("format");				
+						$(this).val(accounting.formatNumber($(this).val()));						
+					});
+					$("#downPayment").change(function(){						
+						$(this).val(accounting.formatNumber($(this).val()));						
+					});
+					$("#loanAmount").change(function(){						
+						$(this).val(accounting.formatNumber($(this).val()));						
+					});
+
+				});
 		})
 		//
 		this.get("#/create", function(context) {
 			var context = this;
+			context.data = {};
 			this.title("Create Letter");
 			this.load("data/users.json")
-				.then(function(users) {
-					context.users = users;				
+				.then(function(users) {					
+					context.data.users = users;				
 				})
-				.render('templates/create.handlebars', context.users)
+				.render('templates/create.handlebars', context.data)
 				.swap()
 				.then(function(partial){
 					//console.log(partial.$("#newClientModelFormSubmitBtn"));
@@ -188,25 +246,24 @@
 
 
 /*
-*
+* Utils 
 */
 
+window.Handlebars.registerHelper('select', function( value, options ){
+        var $el = $('<select />').html( options.fn(this) );
+        $el.find('[value=\"' + value + '\"]').attr({'selected':'selected'});
+        return $el.html();
+    });
 
-var BASE64_MARKER = ';base64,';
+window.Handlebars.registerHelper('currency', function(number) {
+  return accounting.formatMoney(number);
+});
 
-function convertDataURIToBinary(dataURI) {
-  var base64Index = dataURI.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
-  var base64 = dataURI.substring(base64Index);
-  var raw = window.atob(base64);
-  var rawLength = raw.length;
-  var array = new Uint8Array(new ArrayBuffer(rawLength));
-
-  for(i = 0; i < rawLength; i++) {
-    array[i] = raw.charCodeAt(i);
-  }
-  return array;
-}
-
+window.Handlebars.registerHelper('commas', function(number) {
+	if(!number)
+		return "";
+  return accounting.formatNumber(number);
+});
     
 
 //TODO: this should be moved to nav module
