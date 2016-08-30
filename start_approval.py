fede4078d8e3388e5e6a8544a86d3e15d99d9c8c
elvezpablo <prangel@gmail.com>
2016-08-30 06:08:17 -0700

    cleaningup for old-new code

--- start_approval.py
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-/usr/local/bin/python2.7 "/Users/paul.rangel/Library/Application Support/IntelliJIdea12/python/helpers/pydev/pydevd.py" --multiproc --client 127.0.0.1 --port 54852 --file /Applications/GoogleAppEngineLauncher.app/Contents/Resources/GoogleAppEngine-default.bundle/Contents/Resources/google_appengine/dev_appserver.py .
